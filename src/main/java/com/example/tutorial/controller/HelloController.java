6e4f22bf135dc5867204490534c35912e6725a32
ywbest.park <ywbest.park@gmail.com>
2021-12-04 16:23:01 +0900

    :new: Hello-World-페이지-만들기
    new file : HelloController.java
    new file : hello.mustache

---
+++ src/main/java/com/example/tutorial/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.tutorial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("username", "싸이언");
+        return "hello";
+    }
+}
