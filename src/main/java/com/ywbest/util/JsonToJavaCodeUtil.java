dbbe33d237036db03c61491cc66a435fabf90eda
ywbest.park <ywbest.park@gmail.com>
2021-12-12 16:48:45 +0900

    :new: Json To JavaCode
    new file : JsonToJavaCodeUtil.java
    new file : JsonToJavaCodeUtilTest.java

--- src/main/java/com/ywbest/util/JsonToJavaCodeUtil.java
+++ src/main/java/com/ywbest/util/JsonToJavaCodeUtil.java
@@ -25,11 +25,25 @@ public class JsonToJavaCodeUtil {
 
     private LinkedHashMap<String, List<String>> traverseJsonNode(JsonNode jsonNode, LinkedHashMap<String, List<String>> objectMapList, List<String> lineList) {
 
-        if(jsonNode.isObject()){
+        if(jsonNode.isObject()){//오브젝트 일 경우 오브젝트를 구성하는 필드들을 추출
             Iterator<Map.Entry<String, JsonNode>> fields = jsonNode.fields();
             while (fields.hasNext()){
-                Map.Entry<String, JsonNode> entry = fields.next();
+                Map.Entry<String, JsonNode> entry = fields.next();//오브젝트를 구성하는 1개 필드 추출
 
+                /*
+                * 추출한 필드의 Value를 이용하여 필드 타입 구분
+                * 1. 필드 Value가 오브젝트일 경우 :
+                *    1-1. 오브젝트 필드 추가(private A a;)
+                *    1-2. 재귀 함수를 이용하여 계속 순회
+                * 2. 필드 Value가 배열일 경우 :
+                *   2-1. 배열을 1개 추출
+                *   2-1-1. 추출한 배열이 오브젝트일 경우 필드 추가(private List<A> a)
+                *   2-1-2. 추출한 배열이 또 배열일 경우는 문법적으로 오류라고 인식하고 오류 리턴
+                *   2-1-3. 추출한 배열이 Primary일 경우 필드 추가(private List<Long> a)
+                *   2-3. 재귀 함수를 이용하여 계속 순회
+                * 3. 필드 Value가 Primary일 경우 :
+                *   3-1. Primary 필드 추가(private Long a)
+                * */
                 if(entry.getValue().getNodeType() == JsonNodeType.OBJECT){
                     lineList.add("private "+entry.getKey()+" "+entry.getKey()+"; \n");
 
@@ -56,7 +70,7 @@ public class JsonToJavaCodeUtil {
                     lineList.add("private "+getRealType(entry.getValue())+" "+entry.getKey()+"; \n");
                 }
             }
-        }else if(jsonNode.isArray()){
+        }else if(jsonNode.isArray()){// 제일 바깥쪽에 배열이 오는 경우 때문에...
             Iterator<JsonNode> jsonNodeIterator = jsonNode.iterator();
             while (jsonNodeIterator.hasNext()){
                 JsonNode currentNode = jsonNodeIterator.next();
