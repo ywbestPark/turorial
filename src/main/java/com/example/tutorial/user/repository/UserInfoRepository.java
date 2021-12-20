8ffb5037d0d5db30a5149033c349f805fc3d5f21
ywbest.park <ywbest.park@gmail.com>
2021-12-20 22:29:01 +0900

    :new: image lists - image view security 적용

--- src/main/java/com/example/tutorial/user/repository/UserInfoRepository.java
+++ src/main/java/com/example/tutorial/user/repository/UserInfoRepository.java
@@ -1,6 +1,5 @@
 package com.example.tutorial.user.repository;
 
-
 import com.example.tutorial.user.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/tutorial/user/service/UserDetailsImpl.java
+++ src/main/java/com/example/tutorial/user/service/UserDetailsImpl.java
@@ -4,7 +4,6 @@ import com.example.tutorial.user.entity.UserInfo;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
